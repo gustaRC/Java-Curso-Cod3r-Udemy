a7564cadf428bb778bf3f7d1a149083ac392b0b6
Gustavo Rodrigues <gustavorcprofissional@gmail.com>
2025-06-06 16:35:36 -0300

    Iniciado aprendizado sobre Conversão de Tipos primitivos

---
+++ exercicios/src/fundamentos/ConversaoTipoPrimitivoNumerico.java
@@ -0,0 +1,25 @@
+package fundamentos;
+
+public class ConversaoTipoPrimitivoNumerico {
+
+	public static void main(String[] args) {
+		
+		//Converção IMPLICITA
+		double a = 1; //convertido literal int 1 para double (tipo da variável)
+		System.out.println(a);
+		
+		//Atribuindo tipo valor
+		float b = 1.0F; //EXPLICITANDO que 1.0 é float, logo o Java não converte nem interpreta diferente disso
+		
+		//Conversão EXPLICITA - cast
+		float b_cast = (float) 1.234; //O Java CONVERTE o valor que seria naturalmente double, para float
+		
+		//Perca de informação
+		var valor = 10.2344445539202;
+		double d = valor;
+		System.out.println("double: " + d);
+		
+		float f = (float) valor;
+		System.out.println("cast float: " + f);
+	}
+}
