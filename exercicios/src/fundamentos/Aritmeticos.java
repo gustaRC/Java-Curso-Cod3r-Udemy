3f9c56e83db0987f56e1bdb0ef865a4635db4fa2
Gustavo Rodrigues <gustavorcprofissional@gmail.com>
2025-06-09 12:14:59 -0300

    Concluído aula sobre Operadores Aritmeticos

---
+++ exercicios/src/fundamentos/Aritmeticos.java
@@ -0,0 +1,31 @@
+package fundamentos;
+
+public class Aritmeticos {
+
+	public static void main(String[] args) {
+
+		System.out.println(2 + 3);
+
+		// Tipo double
+		var x = 35.04;
+		double y = 2.7;
+
+		System.out.println(x + y);
+		System.out.println(x - y);
+		System.out.println(x * y);
+		System.out.println(x / y);
+
+		System.out.println(); // pular linha
+
+		// Tipo int
+		int a = 10;
+		int b = 4;
+
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println(a / (double) b);
+		System.out.println(a % b); // resto da divisão
+	}
+}
