ba379e0e89a20e3777281da198d482c058c1ae8a
Gustavo Rodrigues <123694354+gustaRC@users.noreply.github.com>
2025-06-10 14:43:25 -0300

    Concluído Desafio Calculadora

--- exercicios/src/fundamentos/DesafioCalculadora.java
+++ exercicios/src/fundamentos/DesafioCalculadora.java
@@ -9,16 +9,13 @@ public class DesafioCalculadora {
 		double valor1 = Double.parseDouble(JOptionPane.showInputDialog("Digite o primeiro número:"));
 		double valor2 = Double.parseDouble(JOptionPane.showInputDialog("Digite o segundo número:"));
 		String operador = JOptionPane.showInputDialog("Digite a operação desejada(+, -, *, /, %):");
-		
-		double calculoSoma = valor1 + valor2;
-		double calculoSub = valor1 - valor2;
-		double calculoMult = valor1 * valor2;
-		double calculoDiv = valor1 / valor2;
-		double calculoMod = valor1 % valor2;
-		
-		System.out.println(operador);
-		
-//		double resultado = operador.equals("+") ? calculoSoma : ; 
-		
+
+		double resultado = operador.equals("+") ? (valor1 + valor2)
+				: (operador.equals("-") ? (valor1 - valor2)
+						: (operador.equals("*") ? (valor1 * valor2) 
+								: (operador.equals("/") ? (valor1 / valor2) : (valor1 % valor2) )));
+
+		System.out.printf("Operação: %.3f %s %.3f = %.3f",
+				valor1, operador, valor2, resultado);
 	}
 }
