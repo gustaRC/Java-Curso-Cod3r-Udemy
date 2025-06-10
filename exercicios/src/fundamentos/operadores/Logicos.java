1e5502e6ac01519f94b9259b5585daea16f35dd1
Gustavo Rodrigues <gustavorcprofissional@gmail.com>
2025-06-10 09:31:14 -0300

    Concluído aula sobre os Operadores Lógicos

--- exercicios/src/fundamentos/operadores/Logicos.java
+++ exercicios/src/fundamentos/operadores/Logicos.java
@@ -3,7 +3,33 @@ package fundamentos.operadores;
 public class Logicos {
 
 	public static void main(String[] args) {
-		
-	}
 
+		boolean condicao1 = true;
+		boolean condicao2 = 3 > 7; // == false
+
+		System.out.println("E- &&: " + (condicao1 && condicao2));
+		System.out.println("OU- ||: " + (condicao1 || condicao2));
+		System.out.println("OU EXCLUSIVO- ^: " + (condicao1 ^ condicao2));
+		System.out.println("NEGAÇÃO- !: " + !condicao1);
+		System.out.println("NEGAÇÃO- !: " + !condicao2);
+
+		// Tabelas verdades
+		System.out.println("\nTabela Verdade E/AND (&&)");
+		System.out.println(true && true);
+		System.out.println(true && false);
+		System.out.println(false && true);
+		System.out.println(false && false);
+
+		System.out.println("\nTabela Verdade OU/OR (||)");
+		System.out.println(true || true);
+		System.out.println(true || false);
+		System.out.println(false || true);
+		System.out.println(false || false);
+
+		System.out.println("\nTabela Verdade NEGAÇÃO/NOT (!)");
+		System.out.println(!true);
+		System.out.println(!!true);
+		System.out.println(!false);
+		System.out.println(!!false);
+	}
 }
