c9a2b3e26e0d4eb5f636767d7592e57da00aa989
Gustavo Rodrigues <123694354+gustaRC@users.noreply.github.com>
2025-06-12 14:40:52 -0300

    Concluído aula sobre a Classe Produto

---
+++ exercicios/src/classe/ProdutoInstanciado.java
@@ -0,0 +1,24 @@
+package classe;
+
+public class ProdutoInstanciado {
+
+	public static void main(String[] args) {
+		
+		Produto p1 = new Produto();
+		p1.nome = "Notebook";
+		p1.preco = 4490.90;
+		p1.desconto = 0.25;
+		
+		var p2 = new Produto();
+		p2.nome = "Caneca Preta";
+		p2.preco = 12.50;
+		p2.desconto = 0.29;
+		
+		System.out.println(p1.nome);
+		
+		double precoFinal1 = p1.preco * (1 - p1.desconto);
+		double precoFinal2 = p2.preco * (1 - p2.desconto);
+		
+		System.out.printf("Média do Carrinho: R$%.2f", (precoFinal1 + precoFinal2) / 2);
+	}
+}
