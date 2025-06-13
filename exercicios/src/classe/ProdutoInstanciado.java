430bb4b38494dc3cab13b640993aa296b642cea6
Gustavo Rodrigues <123694354+gustaRC@users.noreply.github.com>
2025-06-13 16:01:18 -0300

    Concluído aula sobre os Construtores

--- exercicios/src/classe/ProdutoInstanciado.java
+++ exercicios/src/classe/ProdutoInstanciado.java
@@ -4,10 +4,7 @@ public class ProdutoInstanciado {
 
 	public static void main(String[] args) {
 		
-		Produto p1 = new Produto();
-		p1.nome = "Notebook";
-		p1.preco = 4490.90;
-		p1.desconto = 0.25;
+		Produto p1 = new Produto("Notebook", 4490.90, 0.25);
 		
 		var p2 = new Produto();
 		p2.nome = "Caneca Preta";
@@ -15,6 +12,7 @@ public class ProdutoInstanciado {
 		p2.desconto = 0.29;
 		
 		System.out.println(p1.nome);
+		System.out.println(p2.nome);
 		
 		double precoFinal1 = p1.precoComDesconto();
 		double precoFinal2 = p2.precoComDesconto();
