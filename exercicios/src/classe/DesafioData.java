b06b73b95a5c30e22354a0920d5985445989ec8d
Gustavo Rodrigues <123694354+gustaRC@users.noreply.github.com>
2025-06-13 16:06:16 -0300

    Implementado exercício sobre Construtores na classe DesafioData

--- exercicios/src/classe/DesafioData.java
+++ exercicios/src/classe/DesafioData.java
@@ -6,6 +6,18 @@ public class DesafioData {
 	int mes;
 	int ano;
 	
+	DesafioData() {
+		dia = 1;
+		mes = 1;
+		ano = 1970;
+	}
+	
+	DesafioData(int diaInicial, int mesInicial, int anoInicial) {
+		dia = diaInicial;
+		mes = mesInicial;
+		ano = anoInicial;
+	}
+	
 	String obterDataFormatada() {
 		return String.format("%d/%d/%d", dia, mes, ano);
 	}
--- exercicios/src/classe/DesafioDataInstanciada.java
+++ exercicios/src/classe/DesafioDataInstanciada.java
@@ -5,14 +5,8 @@ public class DesafioDataInstanciada {
 	public static void main(String[] args) {
 
 		DesafioData data1 = new DesafioData();
-		data1.dia = 02;
-		data1.mes = 12;
-		data1.ano = 2004;
 		
-		DesafioData data2 = new DesafioData();
-		data2.dia = 25;
-		data2.mes = 05;
-		data2.ano = 2013;
+		DesafioData data2 = new DesafioData(25, 05, 2013);
 		
 		System.out.printf("Data 1: %s", 
 				data1.obterDataFormatada());
