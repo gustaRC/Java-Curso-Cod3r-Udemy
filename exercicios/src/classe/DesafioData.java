9c24bca5314491c3faf6d2d9553de98f8e232285
Gustavo Rodrigues <gustavorcprofissional@gmail.com>
2025-06-16 15:02:19 -0300

    Concluído aula sobre This e This()

--- exercicios/src/classe/DesafioData.java
+++ exercicios/src/classe/DesafioData.java
@@ -2,23 +2,30 @@ package classe;
 
 public class DesafioData {
 
-	int dia;
+	int dia; //variavel de instancia
+//	static int dia; //variavel de classe
 	int mes;
 	int ano;
 	
 	DesafioData() {
-		dia = 1;
-		mes = 1;
-		ano = 1970;
+//		dia = 1;
+//		mes = 1;
+//		ano = 1970;
+		this(1, 1, 1970); //chamando o outro método
 	}
 	
-	DesafioData(int diaInicial, int mesInicial, int anoInicial) {
-		dia = diaInicial;
-		mes = mesInicial;
-		ano = anoInicial;
+	DesafioData(int dia, int mes, int ano) {
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
 	}
 	
 	String obterDataFormatada() {
-		return String.format("%d/%d/%d", dia, mes, ano);
+		String formato = "%d/%d/%d"; //variavel local
+		return String.format(formato, dia, mes, ano);
+	}
+	
+	void imprimirDataFormatada() {
+		System.out.println(this.obterDataFormatada());
 	}
 }
