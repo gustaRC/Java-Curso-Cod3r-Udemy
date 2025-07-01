890a92ab9626320d8a2c378d3c80a34e013b1c42
Gustavo Rodrigues <123694354+gustaRC@users.noreply.github.com>
2025-07-01 16:45:54 -0300

    Concluído primeiro aula sobre Herança

--- exercicios/src/colecoes/Hash.java
+++ exercicios/src/colecoes/Hash.java
@@ -7,11 +7,11 @@ public class Hash {
 	public static void main(String[] args) {
 
 		HashSet<Usuario> usuarios = new HashSet<Usuario>();
-		
+
 		usuarios.add(new Usuario("Pedro"));
 		usuarios.add(new Usuario("Ana"));
 		usuarios.add(new Usuario("Guilherme"));
-		
+
 		System.out.println(usuarios.contains(new Usuario("Guilherme")));
 	}
 }
--- exercicios/src/oo/heranca/Jogo.java
+++ exercicios/src/oo/heranca/Jogo.java
@@ -4,18 +4,28 @@ public class Jogo {
 
 	public static void main(String[] args) {
 
-		Jogador j1 = new Jogador();
+		Monstro j1 = new Monstro();
 		j1.x = 10;
-		j1.y = 10;
-		
-		j1.andar(Direcao.NORTE);
-		j1.andar(Direcao.SUL);
-		j1.andar(Direcao.SUL);
-		j1.andar(Direcao.LESTE);
-		j1.andar(Direcao.LESTE);
-		j1.andar(Direcao.LESTE);
-		
-		System.out.println("X (Leste/Oeste): " + j1.x);
-		System.out.println("Y (Norte/Sul): " + j1.y);
+		j1.y = 11;
+
+		Jogador j2 = new Heroi();
+		j2.x = 10;
+		j2.y = 10;
+
+//		System.out.println("X (Leste/Oeste): " + j1.x);
+//		System.out.println("Y (Norte/Sul): " + j1.y);
+		System.out.println("Vida J1: " + j1.vida);
+		System.out.println("Vida J2: " + j2.vida);
+
+		j1.atacar(j2);
+
+		System.out.println("Vida J1: " + j1.vida);
+		System.out.println("Vida J2: " + j2.vida);
+
+		j2.andar(Direcao.SUL);
+		j1.atacar(j2);
+
+		System.out.println("Vida J1: " + j1.vida);
+		System.out.println("Vida J2: " + j2.vida);
 	}
 }
