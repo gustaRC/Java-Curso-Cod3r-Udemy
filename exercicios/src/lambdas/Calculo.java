588f6a83049d17281173737e9941df58f4f4fdbc
Gustavo Rodrigues <123694354+gustaRC@users.noreply.github.com>
2025-08-12 14:55:08 -0300

    Concluído aula sobre Primeira função lambda

---
+++ exercicios/src/lambdas/Calculo.java
@@ -0,0 +1,8 @@
+package lambdas;
+
+public interface Calculo {
+
+//	public e abstract não são obrigatorios, pois implicitamente
+//	todo método de uma interface é abstrato e público
+	public abstract double executar(double a, double b);
+}
