85723e7e4b85505e7f2a2c4b3f9ad953f8a77af0
Gustavo Rodrigues <gustavorcprofissional@gmail.com>
2025-08-20 14:15:28 -0300

    Concluído segunda aula sobre Foreach funcional

--- exercicios/src/lambdas/Foreach.java
+++ exercicios/src/lambdas/Foreach.java
@@ -7,18 +7,27 @@ public class Foreach {
 
 	public static void main(String[] args) {
 
-		List<String> aprovados = Arrays
-				.asList("Ana", "Bia", "Lia", "Gui");
-		
+		List<String> aprovados = Arrays.asList("Ana", "Bia", "Lia", "Gui");
+
 		System.out.println("Forma tradicional...");
 		for (String nome : aprovados) {
 			System.out.println(nome);
 		}
-		
+
 		System.out.println("\nLambda #01");
-		aprovados.forEach(nome -> System.out.println(nome)); //Igual ao JS
-		
-		System.out.println("\nMethod Reference...");
+		aprovados.forEach(nome -> System.out.println(nome)); // Igual ao JS
+
+		System.out.println("\nMethod Reference #01...");
 		aprovados.forEach(System.out::println);
+
+		System.out.println("\nLambda #02");
+		aprovados.forEach(nome -> meuImprimir(nome)); // Igual ao JS
+
+		System.out.println("\nMethod Reference #02...");
+		aprovados.forEach(Foreach::meuImprimir);
+	}
+
+	static void meuImprimir(String nome) {
+		System.out.println("Oi! Meu nome é " + nome);
 	}
 }
