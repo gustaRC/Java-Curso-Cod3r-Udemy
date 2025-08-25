c99c9d0ca31ac3d145ef9b640005affc58f1b716
Gustavo Rodrigues <gustavorcprofissional@gmail.com>
2025-08-25 14:38:32 -0300

    Concluído aula sobre a interface Function

--- exercicios/src/lambdas/Consumidor.java
+++ exercicios/src/lambdas/Consumidor.java
@@ -1,6 +1,8 @@
 package lambdas;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -8,7 +10,7 @@ public class Consumidor {
 
 	public static void main(String[] args) {
 
-		// Recebe um parametro e retorna void
+		// Recebe um único parametro e retorna void
 		Consumer<Produto> imprimirNome = prod -> System.out.println(prod.nome);
 
 		Produto p1 = new Produto("Caneta Azul", 12.30, 0.09);
@@ -19,10 +21,22 @@ public class Consumidor {
 		Produto p4 = new Produto("Caneta Vermelha", 12.30, 0.09);
 		Produto p5 = new Produto("Caneta Amarela", 12.30, 0.09);
 
+//		List === Interface
 		List<Produto> produtos = Arrays.asList(p1, p2, p3, p4, p5);
 
 		produtos.forEach(imprimirNome);
 		produtos.forEach(p -> System.out.println(p.preco));
 		produtos.forEach(System.out::println); // necessário configurar toString() para formatação na exibição
+
+		
+		List<String> variavelList = new ArrayList<String>();
+		
+		variavelList.addAll(Arrays.asList("nome1", "nome2"));
+		variavelList = new LinkedList<String>(); // adiciona uma nova referencia na memória, perdendo dados antigos
+		variavelList.forEach(System.out::println);
+
+		variavelList.addAll(Arrays.asList("nome3", "nome4"));
+		variavelList.forEach(System.out::println);
+
 	}
 }
--- exercicios/src/lambdas/Predicado.java
+++ exercicios/src/lambdas/Predicado.java
@@ -6,6 +6,7 @@ public class Predicado {
 
 	public static void main(String[] args) {
 
+//		Recebe um único paramêtro e retorna boolena
 		Predicate<Produto> isCaro = prod -> (prod.preco * (1 - prod.desconto)) > 750;
 
 		Produto produto = new Produto("Notebook", 2390, 0.15);
