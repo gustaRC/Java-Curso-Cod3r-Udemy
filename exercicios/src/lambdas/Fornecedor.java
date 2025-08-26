dd80b0092a470933125a2d57c7a0907037438947
Gustavo Rodrigues <gustavorcprofissional@gmail.com>
2025-08-26 14:50:13 -0300

    Concluído aula sobre o Supplier

---
+++ exercicios/src/lambdas/Fornecedor.java
@@ -0,0 +1,17 @@
+package lambdas;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class Fornecedor {
+
+	public static void main(String[] args) {
+
+//		Não recebe nenhum parametro e retorna alguma coisa
+//		Define o valor de retorno 
+		Supplier<List<String>> umaLista = () -> Arrays.asList("Gu", "Gui", "Go");
+		
+		System.out.println(umaLista.get());
+	}
+}
