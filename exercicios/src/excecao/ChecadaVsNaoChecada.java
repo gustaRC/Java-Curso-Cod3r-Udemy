c8a0a3097aaf8130d2d441e202c2a428fb00882d
Gustavo Rodrigues <123694354+gustaRC@users.noreply.github.com>
2025-10-28 16:47:36 -0300

    Concluído primeira aula sobre Exceção Tratada e Não Tratada

---
+++ exercicios/src/excecao/ChecadaVsNaoChecada.java
@@ -0,0 +1,37 @@
+package excecao;
+
+public class ChecadaVsNaoChecada {
+
+	public static void main(String[] args) {
+
+		try {
+			geraErro1();
+		} catch (RuntimeException e) {
+			System.out.println(e.getMessage());
+		}
+
+//		se torna obrigatorio o tratamento
+		try {
+			geraErro2();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		System.out.println("Fim :)");
+	}
+
+//	Exceçao NÃO checada ou NÃO verificada.
+	static void geraErro1() {
+		throw new RuntimeException("Ocorreu um erro! #01");
+//		throw somente funciona com heranças da classe Throwable
+//		ao utilizar o throw estamos interrompendo o fluxo da aplicação
+	}
+
+//	Exceçao checada ou verificada.
+//	O 'throws Exception' é como se estivessemos 
+//	dizendo que este método não se responsabiliza tratamento do erro
+	static void geraErro2() throws Exception {
+		throw new Exception("Ocorreu um erro! #02");
+	}
+
+}
