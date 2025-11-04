73815ef10c3d6b21f72acb9ecee516b207446821
Gustavo Rodrigues <gustavorcprofissional@gmail.com>
2025-11-04 15:40:54 -0300

    Concluído aula sobre exceções causadoras

---
+++ exercicios/src/excecao/Causa.java
@@ -0,0 +1,31 @@
+package excecao;
+
+public class Causa {
+
+	public static void main(String[] args) {
+		
+		try {
+			metodoA(null);			
+		} catch (Exception e) {
+			if (e.getCause() != null ) {
+				System.out.println(e.getCause().getMessage());				
+			}
+		}
+	}
+	
+	static void metodoA(Aluno aluno) {
+		try {
+			metodoB(aluno);			
+		} catch (Exception e) {
+			throw new IllegalArgumentException("Erro no meio do caminho: " + e.getMessage());
+		}
+	}
+	
+	static void metodoB(Aluno aluno) {
+		if(aluno == null) {
+			throw new NullPointerException("Paramêtro está NULO!"); // exceção NÃO tratada
+		}
+		
+		System.out.println(aluno.nome);
+	}
+}
