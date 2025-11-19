79270bcb6bf91406fa8e4e9e26035e88ae728951
Gustavo Rodrigues <gustavorcprofissional@gmail.com>
2025-11-19 08:40:48 -0300

    Adicionado anotações no Tipo String Equals

--- exercicios/src/fundamentos/TipoStringEquals.java
+++ exercicios/src/fundamentos/TipoStringEquals.java
@@ -12,6 +12,9 @@ public class TipoStringEquals {
 		System.out.println("2" == s); // == false
 		System.out.println(s2 == s); // == false
 		// Prefira utilizar equals para String, ao inves do ==
+		// Isso acontece pois o Java irá comparar a referencia na mémoria, 
+		// logo ao lidar com Objetos (como a String) irá dar valores diferentes na referencia,
+		// porque não é atribuido diretamente seu valor, mas sim a referencia do Objeto
 		System.out.println("2".equals(s)); // == true
 	}
 }
