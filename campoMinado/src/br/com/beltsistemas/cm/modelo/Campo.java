6dd6b0b94f21523b7f1ca1811d433e44dae52c74
Gustavo Rodrigues <gustavorcprofissional@gmail.com>
2025-11-24 15:22:28 -0300

    Criado função que adicionará os campos vizinhos. Criado classe teste para a classe Campo

--- campoMinado/src/br/com/beltsistemas/cm/modelo/Campo.java
+++ campoMinado/src/br/com/beltsistemas/cm/modelo/Campo.java
@@ -14,12 +14,26 @@ public class Campo {
 
 	private List<Campo> vizinhos = new ArrayList<Campo>();
 
-	Campo(int linha, int coluna) {
+	public Campo(int linha, int coluna) {
 		this.linha = linha;
 		this.coluna = coluna;
 	}
 
-	boolean adicionarVizinho(Campo vizinho) {
-		return false;
+	public boolean adicionarVizinho(Campo vizinho) {
+		boolean linhaDiferente = linha != vizinho.linha;
+		boolean colunaDiferente = coluna != vizinho.coluna;
+		boolean diagonal = linhaDiferente && colunaDiferente;
+
+		int deltaLinha = Math.abs(linha - vizinho.linha);
+		int deltaColuna = Math.abs(coluna - vizinho.coluna);
+		int deltaGeral = deltaColuna + deltaLinha;
+
+		if ((deltaGeral == 1 && !diagonal) || (deltaGeral == 2 && diagonal)) {
+			vizinhos.add(vizinho);
+			return true;
+		} else {
+			return false;
+		}
 	}
+
 }
