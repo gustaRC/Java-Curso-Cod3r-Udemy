f5b64666441b513f1ade58514b75d2da06836d42
Gustavo Rodrigues <gustavorcprofissional@gmail.com>
2025-11-24 14:32:25 -0300

    Implementado atributos da classe Campo

--- campoMinado/src/br/com/beltsistemas/cm/modelo/Campo.java
+++ campoMinado/src/br/com/beltsistemas/cm/modelo/Campo.java
@@ -1,5 +1,25 @@
 package br.com.beltsistemas.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Campo {
 
+	private final int linha;
+	private final int coluna;
+
+	private boolean aberto; // por padrão, o valor de boolean quando não atribuido é 'false'
+	private boolean minado = false;
+	private boolean marcado = false;
+
+	private List<Campo> vizinhos = new ArrayList<Campo>();
+
+	Campo(int linha, int coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+
+	boolean adicionarVizinho(Campo vizinho) {
+		return false;
+	}
 }
