8893f8faec17ad1d15e2b0fadd50c5602a2cdfc3
Gustavo Rodrigues <123694354+gustaRC@users.noreply.github.com>
2026-03-18 16:02:52 -0300

    Concluido aula 1 sobre interface grafica do campo minado

---
+++ campo-minado-swing/src/br/com/beltsistemas/cm/visao/PainelTabuleiro.java
@@ -0,0 +1,12 @@
+package br.com.beltsistemas.cm.visao;
+
+import javax.swing.JPanel;
+
+import br.com.beltsistemas.cm.modelo.Tabuleiro;
+
+public class PainelTabuleiro extends JPanel{
+	
+	public PainelTabuleiro(Tabuleiro tabuleiro) {
+		
+	}
+}
