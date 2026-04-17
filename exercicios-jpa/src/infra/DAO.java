3986ac93f600385203ba24a04e14d8a176abc901
Gustavo Rodrigues <gustavorcprofissional@gmail.com>
2026-04-17 14:51:48 -0300

    Concluído aula sobre DAO com JPA

---
+++ exercicios-jpa/src/infra/DAO.java
@@ -0,0 +1,69 @@
+package infra;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class DAO<E> {
+
+    private static EntityManagerFactory emf;
+    private EntityManager em;
+    private Class<E> classe;
+
+    static { //diferente de inicializar variáveis no momento da instancia (no constructor), aqui será carregado somente 1 vez quando o Java carregar o arquivo
+        try {
+            emf = Persistence.createEntityManagerFactory("exercicios-jpa");
+        } catch (Exception e) {
+            // log/logar -> log4j
+        }
+    }
+
+    public DAO() {
+        this(null);
+    }
+
+    public DAO(Class<E> classe) {
+        this.classe = classe;
+        em = emf.createEntityManager();
+    }
+
+    public DAO<E> abrirTransacao() {
+        em.getTransaction().begin();
+        return this;
+    }
+
+    public DAO<E> fecharTransacao() {
+        em.getTransaction().commit();
+        return this;
+    }
+
+    public DAO<E> incluir(E entidade) {
+        em.persist(entidade);
+        return this;
+    }
+
+    public DAO<E> incluirAtomico(E entidade) {
+        return this.abrirTransacao().incluir(entidade).fecharTransacao();
+    }
+
+    public List<E> obterTodos() {
+        return this.obterTodos(15, 0);
+    }
+
+    public List<E> obterTodos(int qtde /*limit*/, int deslocamento /*offset*/) {
+        if (classe == null) {
+            throw new UnsupportedOperationException("Classe nula.");
+        }
+
+        String jpql = "select e from " + classe.getName() + " e";
+
+        TypedQuery<E> query = em.createQuery(jpql, classe);
+        query.setMaxResults(qtde);
+        query.setFirstResult(deslocamento);
+
+        return query.getResultList();
+    }
+
+}
