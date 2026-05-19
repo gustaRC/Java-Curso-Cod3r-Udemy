c385226bec03d130e6985ad14d9a342f14e7a143
Gustavo Rodrigues <123694354+gustaRC@users.noreply.github.com>
2026-05-19 20:28:07 -0300

    Concluído primeira aula sobre o uso de parametros na requisição

--- exerciciosSB/src/main/java/br/com/beltsistemas/exerciciosSB/controllers/ClienteController.java
+++ exerciciosSB/src/main/java/br/com/beltsistemas/exerciciosSB/controllers/ClienteController.java
@@ -2,6 +2,7 @@ package br.com.beltsistemas.exerciciosSB.controllers;
 
 import br.com.beltsistemas.exerciciosSB.models.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,10 @@ public class ClienteController {
     @GetMapping("qualquer2") //podemos declarar explicitamente o 'path = ' ou somente o valor do path diretamente
     public Cliente obterCliente2() {
         return new Cliente(9, "Siricuticu", "999.888.777-00");
+    }
+
+    @GetMapping("{id}") //path = "/clientes/[:ID]"
+    public Cliente obterClientePorId(@PathVariable int id) { //converterá o valor digitado para int
+        return new Cliente(id, "Biricubico e Siricuticu", "123.456.789-00");
     }
 }
